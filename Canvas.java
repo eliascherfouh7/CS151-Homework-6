d61f76cf6ff5a5a289003938f0b8bf6a4b215fa1
ntang58 <ntang58@yahoo.com>
2016-05-06 14:41:34 -0700

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -1,11 +1,13 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 
 public class Canvas extends JPanel{
+	private DShape[] shapes;
 	/**
 	 * Extends JPanel. The layout of the Panel is following a FlowLayout
 	 */
@@ -13,5 +15,20 @@ public class Canvas extends JPanel{
 		super(new FlowLayout());
 		this.setPreferredSize(new Dimension(400, 400));
 		this.setBackground(Color.WHITE);
+	}
+	/**
+	 * draws all the components in the canvas
+	 */
+	public void paintComponenet(){
+		for(DShape ds : shapes){
+			ds.draw();
 		}
+	}
+	public void addDShape(DShape d){
+		
+	}
+	public void removeDShape(DShape d ){
+		
+	}
+	
 }
